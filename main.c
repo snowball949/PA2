5d85b7bb6128f1c17c684e69cb13282576790b6b
snowball949 <49416665+snowball949@users.noreply.github.com>
2020-10-08 03:02:31 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,58 +1,52 @@
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
+#include <linux/init.h>           
+#include <linux/module.h>         
+#include <linux/device.h>         
+#include <linux/kernel.h>
+#include <linux/fs.h>            
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/errno.h>
 #include <linux/types.h>
 #include <linux/proc_fs.h>
-#include <linux/fcntl.h>          // Required for the copy to user function
+#include <linux/fcntl.h>          
 #define  DEVICE_NAME "main_device"            
  
-MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Jack Dickinson");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");            ///< A version number to inform users
+MODULE_LICENSE("GPL");           
+MODULE_AUTHOR("Jack Dickinson");    
+         
  
 int   majorNumber;     
-int majorNum = 473;                ///< Stores the device number -- determined automatically
-char*   device_buffer;                ///< Memory for the string that is passed from userspace
-short  size_of_message;              ///< Used to remember the size of the string stored
-int    numberOpens = 0;              ///< Counts the number of times the device is opened
- ///< The device-driver class struct pointer
- ///< The device-driver device struct pointer
+int majorNum = 473;                
+char*   device_buffer;                
+short  size_of_message;              
+int    numberOpens = 0;              
+
  
-// The prototype functions for the character driver -- must come before the struct definition
+ 
+
  int     my_open(struct inode *, struct file *);
  int     my_close(struct inode *, struct file *);
  ssize_t my_read(struct file *, char *, size_t, loff_t *);
  ssize_t my_write(struct file *, const char *, size_t, loff_t *);
  int main_device_init(void);
  void main_device_exit(void); 
-/** @brief Devices are represented as file structure in the kernel. The file_operations structure from
- *  /linux/fs.h lists the callback functions that you wish to associated with your file operations
- *  using a C99 syntax structure. char devices usually implement open, read, write and release calls
- */
+ loff_t my_seek(struct file*, loff_t, int)
+
  struct file_operations fops =
 {
    .open = my_open,
    .read = my_read,
    .write = my_write,
    .release = my_close,
+   .llseak = my_seek,
 };
  
-/** @brief The LKM initialization function
- *  The static keyword restricts the visibility of the function to within this C file. The __init
- *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
- *  time and that it can be discarded and its memory freed up after that point.
- *  @return returns 0 if successful
- */
- int main_device_init(void){
+
+ int main_device_init(void)
+ {
    printk(KERN_INFO "Main device: Initializing the main_device LKM\n");
  
-   // Try to dynamically allocate a major number for the device -- more difficult but worth it
+   
    majorNumber = register_chrdev(majorNum, DEVICE_NAME, &fops);
    if (majorNumber<0){
       printk(KERN_ALERT "Main device failed to register a major number\n");
@@ -82,11 +76,9 @@ int    numberOpens = 0;              ///< Counts the number of times the device 
      return majorNumber;
 }
  
-/** @brief The LKM cleanup function
- *  Similar to the initialization function, it is static. The __exit macro notifies that if this
- *  code is used for a built-in driver (not a LKM) that this function is not required.
- */
- void main_device_exit(void){
+
+ void main_device_exit(void)
+ {
    unregister_chrdev(majorNum, DEVICE_NAME);
       if(device_buffer)
       {
@@ -95,67 +87,81 @@ int    numberOpens = 0;              ///< Counts the number of times the device 
    printk(KERN_INFO "Main device: Goodbye from the LKM!\n");
 }
  
-/** @brief The device open function that is called each time the device is opened
- *  This will only increment the numberOpens counter in this case.
- *  @param inodep A pointer to an inode object (defined in linux/fs.h)
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- */
- int my_open(struct inode *inodep, struct file *filep){
+
+ int my_open(struct inode *inodep, struct file *filep)
+ {
    numberOpens++;
    printk(KERN_INFO "Main Device: Device has been opened %d time(s)\n", numberOpens);
    return 0;
 }
  
-/** @brief This function is called whenever device is being read from user space i.e. data is
- *  being sent from the device to the user. In this case is uses the copy_to_user() function to
- *  send the buffer string to the user and captures any errors.
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- *  @param buffer The pointer to the buffer to which this function writes the data
- *  @param len The length of the b
- *  @param offset The offset if required
- */
- ssize_t my_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+
+ ssize_t my_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+ {
    int error_count = 0;
    error_count = copy_to_user(buffer, device_buffer, size_of_message);
  
-   if (error_count==0){            // if true then have success
+   if (error_count==0){            
       printk(KERN_INFO "Main device: Sent %d characters to the user\n", size_of_message);
-      return (size_of_message=0);  // clear the position to the start and return 0
+      return (size_of_message=0);  
    }
    else {
       printk(KERN_INFO "Main Device: Failed to send %d characters to the user\n", error_count);
-      return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
+      return -EFAULT;              
    }
 }
+
+
  
-/** @brief This function is called whenever the device is being written to from user space i.e.
- *  data is sent to the device from the user. The data is copied to the message[] array in this
- *  LKM using the sprintf() function along with the length of the string.
- *  @param filep A pointer to a file object
- *  @param buffer The buffer to that contains the string to write to the device
- *  @param len The length of the array of data that is being passed in the const char buffer
- *  @param offset The offset if required
- */
- ssize_t my_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-   sprintf(device_buffer, "%s(%zu letters)", buffer, len);   // appending received string with its length
-   size_of_message = strlen(device_buffer);                 // store the length of the stored message
+
+
+ ssize_t my_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+ {
+   sprintf(device_buffer, "%s(%zu letters)", buffer, len);   
+   size_of_message = strlen(device_buffer);                 
    printk(KERN_INFO "Main Device: Received %zu characters from the user\n", len);
    return len;
 }
  
-/** @brief The device release function that is called whenever the device is closed/released by
- *  the userspace program
- *  @param inodep A pointer to an inode object (defined in linux/fs.h)
- *  @param filep A pointer to a file object (defined in linux/fs.h)
- */
- int my_close(struct inode *inodep, struct file *filep){
+
+ int my_close(struct inode *inodep, struct file *filep)
+ {
    printk(KERN_INFO "Main device: Device successfully closed\n");
    return 0;
-}
+ }
+
+loff_t my_seek(struct file*filep, loff_t offset, int whence)
+{
  
-/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
- *  identify the initialization function at insertion time and the cleanup function (as
- *  listed above)
- */
+ loff_t newpos = lseek(filep, offset,;
+  switch (whence):
+ {
+  case 0: 
+   newpos = lseek(filep, offset, SEEK_SET);
+   break;
+  
+  case 1:
+   newpos = lseek(filep, offset, SEEK_CUR);
+   break;
+  
+  case 2:
+   newpos = lseek(filep, offset, SEEK_END);
+  
+  default:
+   return -EINVAL;
+ }
+                       
+ if (newpos<0)
+  {
+           return -EINVAL;
+  }
+      filep->f_pos = newpos;
+      return newpos;
+                       
+       
+}
+
+ 
+
 module_init(main_device_init);
 module_exit(main_device_exit);
