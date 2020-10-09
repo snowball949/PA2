e13df927c6f5935871db1fdd55acfc8f6eb425a1
snowball949 <49416665+snowball949@users.noreply.github.com>
2020-10-09 02:53:03 -0600

    Update testPro.c

--- testPro.c
+++ testPro.c
@@ -5,35 +5,37 @@
 #include<string.h>
 #include<unistd.h>
  
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
- char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 1024   
+            
+ char* receive;
  char option;
  int bytes;
- string userS;
+ char  userS[BUFFER_LENGTH];
  off_t offset;
  int whence;
   
 int main(){
    int ret, fd;
-   char stringToSend[BUFFER_LENGTH];
-   printf("Starting device test code example...\n");
-   fd = open("/dev/main_device", O_RDWR);             // Open the device with read/write access
+   fd = open("/dev/main_device", O_RDWR);             
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
  while (option != 'e')
  {
+  printf("\n");
+  printf("\n");
   printf("Enter 'r' to tell the program to read a certain number of bytes.\n");
   printf("Enter 'w' to enter data you want to write.\n");
-  printf("Enter 's' to alter second and thrid parameters.\n);
-  printf("Enter 'e' to exit the program.\n);
+  printf("Enter 's' to alter second and thrid parameters.\n");
+  printf("Enter 'e' to exit the program.\n");
   option = getchar();
   if (option == 'r')
          {
            printf("Enter the number of bytes you want to read:\n ");
-           scanf("%d", bytes);
-           ret = read(fd, receive, bytes);
+           scanf("%d",&bytes);
+           receive = malloc(BUFFER_LENGTH);
+           ret = read(fd, receive+offset, bytes);
            if (ret < 0)
            {
             printf("Failed to read file");
@@ -51,15 +53,16 @@ int main(){
            printf("Failed to write to file");
            return errno;
           }
+           printf("You wrote to the file [%s}\n");
          }
     else if (option == 's')
          {
           printf("Enter an offset: \n");
-          scanf("%lld", offset);
+          scanf("%lld", &offset);
           printf("Enter a whence: \n");
-          scanf("%d", whence);
-          ret = lseek(fd, offset, whence);
-          if (ret < 0)
+          scanf("%d",&whence);
+           receive = lseek(fd, offset, whence);
+          if (receive < 0)
           {
            printf("Failed to seek\n");
            return errno;
@@ -67,7 +70,7 @@ int main(){
          }
          else if (option == 'e')
          {
-          printf("You have quit the program");
+          printf("You have quit the program\n");
           return 0;
          }
          else
